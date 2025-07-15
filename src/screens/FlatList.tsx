f42845a65ec8bdcb300ac4193b563ea473778251
Husnain Naseer <m.husnain.naseer@gmail.com>
2025-07-15 19:19:05 +0500

    added scroll to top

--- src/screens/FlatList.tsx
+++ src/screens/FlatList.tsx
@@ -1,9 +1,12 @@
-import React, { useMemo } from 'react';
-import { FlatList, View, Text, Image } from 'react-native';
+import React, { useMemo, useRef, useState } from 'react';
+import { FlatList, View, Text, Image, TouchableOpacity } from 'react-native';
 
 const dummyData = Array.from({ length: 10 }, (_, i) => { return { id: i, title: `Dummy Data ${i}`, url: 'https://img.freepik.com/free-photo/sunset-time-tropical-beach-sea-with-coconut-palm-tree_74190-1075.jpg' } })
 
 export const FeedList = () => {
+
+	const flatListRef = useRef(null);
+	const [showScrollToTop, setShowScrollToTop] = useState(false);
 
 	function renderItem({ item, index }: { item: { id: number, title: string, url: string }, index: number }) {
 		const listItem = useMemo(() => <>
@@ -16,9 +19,18 @@ export const FeedList = () => {
 
 	<FlatList
 		data={dummyData}
+		ref={flatListRef}
+		onScroll={() => setShowScrollToTop(true)}
 		renderItem={renderItem}
 		keyExtractor={({ id }) => id.toString()}
 		ListHeaderComponent={(item) => <Text>{item.title}</Text>}
+		ListFooterComponent={() => showScrollToTop && <TouchableOpacity onPress={() => {
+			flatListRef?.current?.scrollToTop();
+			setShowScrollToTop(false)
+		}}>
+			<Text>{'Move to Top'}</Text>
+		</TouchableOpacity>
+		}
 		stickyHeaderIndices={[0]}
 		initialNumToRender={15}
 		removeClippedSubviews
